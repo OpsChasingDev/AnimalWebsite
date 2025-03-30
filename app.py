1d78931e0565b04a114bdf05e8dc0fadfd33414a
Robert Stapleton <robert@stapleton-family.net>
2025-03-30 11:59:06 -0400

    home page headshots

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+@app.route("/")
+def index():
+    animal_images = os.listdir(os.path.join(app.static_folder, "images"))
+    return render_template("index.html", animal_images=animal_images)
 
-# Required for Azure App Service
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
